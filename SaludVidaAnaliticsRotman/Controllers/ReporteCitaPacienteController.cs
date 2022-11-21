91468bcb54b147657e226dff54a5a6245ebc5b80
Rotman Vargas <rotman.vargas@gmail.com>
2022-11-21 00:09:49 -0600

    prueba reportes

---
+++ SaludVidaAnaliticsRotman/Controllers/ReporteCitaPacienteController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SaludVidaAnaliticsRotman.Models;
+
+namespace SaludVidaAnaliticsRotman.Controllers
+{
+    public class ReporteCitaPacienteController : Controller
+    {
+        private readonly DbsaludVidaContext _context;
+
+        public ReporteCitaPacienteController(DbsaludVidaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+         
+
+            return View();
+            //return View(await _context.Pacientes.ToListAsync());
+        }
+    }
+}
+
+
